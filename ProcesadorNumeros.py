11b60de1fe1ba1919893f1902c9c21f9d7a3c590
pcastellanosandes <pa.castellanos11@uniandes.edu.co>
2017-03-21 22:07:21 -0500

    Paso 2: prueba para cadena de dos numeros y número mínimo (AZUL)

--- ProcesadorNumeros.py
+++ ProcesadorNumeros.py
@@ -3,25 +3,27 @@ __autor__= 'Paula Castellanos'
 class ProcesadorNumeros:
     def procesarListaNumeros (self, cadena):
         resultado = [0]*4
-        resultado[0] = self.calcularCantidadNumeros(cadena)
-        resultado[1] = self.calcularMinimo(cadena)
-        return resultado
-
-    def calcularCantidadNumeros(self, cadena):
-        resultado = 0
-        if cadena != "":
-            numeros = cadena.split(',')
-            resultado = len(numeros)
-        return resultado
-
-    def calcularMinimo(self, cadena):
         numeros = self.convertirAInt(cadena)
+        resultado[0] = self.calcularCantidadNumeros(numeros)
+        resultado[1] = self.calcularMinimo(numeros)
+        return resultado
+
+    def calcularCantidadNumeros(self, numeros):
+        resultado = len(numeros)
+        return resultado
+
+    def calcularMinimo(self, numeros):
         numeros.sort()
-        return numeros[0]
+        if len(numeros) > 0:
+            return numeros[0]
+        else:
+            return 0
 
     def convertirAInt(self, cadena):
+        nums = []
         if cadena != "":
             numeros = cadena.split(',')
-            return [int(numeric_string) for numeric_string in numeros]
-        else:
-            return [0]
+            for num in numeros:
+                nums.append(int(num))
+
+        return  nums
--- TestProcesadorNumeros.py
+++ TestProcesadorNumeros.py
@@ -6,30 +6,19 @@ class TestProcesadorNumeros(TestCase):
     def test_ListaVacia(self):
         resultado = ProcesadorNumeros().procesarListaNumeros("")
         self.assertEqual(resultado[0], 0, "Validar cadena vacia")
+        self.assertEqual(resultado[1], 0, "Obtener minimo de cadena vacia")
 
     def test_ListaUnNumero(self):
         resultado = ProcesadorNumeros().procesarListaNumeros("12")
         self.assertEqual(resultado[0], 1, "Validar cadena un numero")
+        self.assertEqual(resultado[1], 12, "Obtener minimo cadena con un numero")
 
     def test_ListaDosNumeros(self):
         resultado = ProcesadorNumeros().procesarListaNumeros("12,2")
         self.assertEqual(resultado[0], 2, "Validar cadena con dos numeros")
+        self.assertEqual(resultado[1], 2, "Validar minimo de cadena con dos numeros")
 
     def test_ListaNNumeros(self):
         resultado = ProcesadorNumeros().procesarListaNumeros("12,2,44")
         self.assertEqual(resultado[0], 3, "Validar cadena con n  numeros")
 
-    def test_ListaVaciaConMinimo(self):
-        resultado = ProcesadorNumeros().procesarListaNumeros("")
-        self.assertEqual(resultado[0], 0, "Validar cadena con n  numeros")
-        self.assertEqual(resultado[1], 0, "Obtener minimo de cadena vacia")
-
-    def test_ListaUnNumeroConMinimo(self):
-        resultado = ProcesadorNumeros().procesarListaNumeros("12")
-        self.assertEqual(resultado[0], 1, "Validar cadena un numero")
-        self.assertEqual(resultado[1], 12, "Obtener minimo cadena con un numero")
-
-    def test_ListaDosNumerosConMinimo(self):
-        resultado = ProcesadorNumeros().procesarListaNumeros("12,2")
-        self.assertEqual(resultado[0], 2, "Validar cadena con dos numeros")
-        self.assertEqual(resultado[1], 2, "Validar minimo de cadena con dos numeros")
