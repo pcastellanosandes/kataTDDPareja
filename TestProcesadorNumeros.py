2244f5750186f7d50e93e9f2f68f0e135667c90f
flxruizruiz <fe.ruiz@uniandes.edu.co>
2017-03-21 21:12:46 -0500

    Paso 1 - test validar cantidad de número en lista con varios numeros (ROJO)

--- TestProcesadorNumeros.py
+++ TestProcesadorNumeros.py
@@ -14,3 +14,7 @@ class TestProcesadorNumeros(TestCase):
     def test_ListaDosNumeros(self):
         resultado = ProcesadorNumeros().procesarListaNumeros("12,2")
         self.assertEqual(resultado[0], 2, "Validar cadena con dos numeros")
+
+    def test_ListaNNumeros(self):
+        resultado = ProcesadorNumeros().procesarListaNumeros("12,2,44")
+        self.assertEqual(resultado[0], 3, "Validar cadena con n  numeros")
